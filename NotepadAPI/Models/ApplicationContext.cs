ef235125e73f6aa6b9a1c71ccf251149e81f6c85
Dzmitry <dzmi3y@gmail.com>
2018-08-28 14:42:30 +0300

    Added 'ApplicationContext' with interface

---
+++ NotepadAPI/Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotepadAPI.Models
+{
+    public class ApplicationContext:DbContext, IApplicationContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        { }
+
+       public DbSet<Entry> Entries { get; set; }
+       public DbSet<User> Users { get; set; }
+        
+    }
+}
---
+++ NotepadAPI/Models/IApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotepadAPI.Models
+{
+    public interface IApplicationContext
+    {
+        DbSet<Entry> Entries { get; set; }
+        DbSet<User> Users { get; set; }
+        int SaveChanges();
+    }
+}
