1f981c7b9d507224d803359eaa5816a61cf3ddf6
Dzmitry <dzmi3y@gmail.com>
2018-08-28 13:52:24 +0300

    Added Models: 'Entry' and 'User'.

---
+++ NotepadAPI/Models/Entry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotepadAPI.Models
+{
+    public class Entry
+    {
+        public int EntryId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
